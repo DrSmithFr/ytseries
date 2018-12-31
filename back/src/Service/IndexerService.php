03ca9dd2cc4e3878f2f72ab0df7edb6a7ddd50da
John Daligault <johndaligault@gmail.com>
2018-12-31 13:00:36 +0100

    fixing search display

--- back/src/Service/IndexerService.php
+++ back/src/Service/IndexerService.php
@@ -23,14 +23,22 @@ class IndexerService
     {
         $series = $this->repository->getFullyLoadedSeriesById($series->getId());
 
+        $episodes = [];
+        foreach ($series->getSeasons() as $season) {
+            foreach ($season->getEpisodes() as $episode) {
+                $episodes[] = $episode;
+            }
+        }
+
         return new Document(
             $series->getId(), // Manually defined ID
             [
-                'id'   => $series->getId(),
-                'name' => $series->getName(),
-                'image' => $series->getImage() ?? '',
+                'id'          => $series->getId(),
+                'name'        => $series->getName(),
+                'image'       => $series->getImage() ?? '',
                 'description' => $series->getDescription(),
-                'season' => $series->getSeasons()->count()
+                'seasons'     => $series->getSeasons()->count(),
+                'episodes'    => count($episodes),
             ],
             "series" // Types are deprecated, to be removed in Elastic 7
         );
