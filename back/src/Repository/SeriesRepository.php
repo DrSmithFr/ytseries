b8698b452162ed21a792660edaebd8dac629d796
John Daligault <johndaligault@gmail.com>
2019-01-04 21:19:06 +0100

    fixing seriesseason order

--- back/src/Repository/SeriesRepository.php
+++ back/src/Repository/SeriesRepository.php
@@ -60,8 +60,8 @@ class SeriesRepository extends ServiceEntityRepository
             ->leftJoin('season.episodes', 'episode')
             ->where('series.id = :id')
             ->setParameter('id', $id)
-            ->orderBy('season.rank', 'ASC')
-            ->orderBy('episode.rank', 'ASC')
+            ->addOrderBy('season.rank', 'ASC')
+            ->addOrderBy('episode.rank', 'ASC')
             ->getQuery()
             ->getOneOrNullResult();
     }
