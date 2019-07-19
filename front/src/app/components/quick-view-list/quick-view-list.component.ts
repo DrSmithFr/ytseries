755b7aefd2c4d2260648a60e197d047351f602ed
John Daligault <johndaligault@gmail.com>
2019-07-19 14:58:58 +0200

    adding list auto close

--- front/src/app/components/quick-view-list/quick-view-list.component.ts
+++ front/src/app/components/quick-view-list/quick-view-list.component.ts
@@ -1,18 +1,31 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {AssetModel}               from '../../models/search/asset.model';
+import {Component, Input, OnInit, QueryList, ViewChildren} from '@angular/core';
+import {AssetModel}                                        from '../../models/search/asset.model';
+import {QuickViewComponent}                                from '../quick-view/quick-view.component';
 
-@Component({
-  selector: 'app-quick-view-list',
-  templateUrl: './quick-view-list.component.html',
-  styleUrls: ['./quick-view-list.component.css']
-})
+@Component(
+  {
+    selector:    'app-quick-view-list',
+    templateUrl: './quick-view-list.component.html',
+    styleUrls:   ['./quick-view-list.component.css']
+  }
+)
 export class QuickViewListComponent implements OnInit {
 
   @Input() series: AssetModel[];
 
-  constructor() { }
+  @ViewChildren(QuickViewComponent) quickViews: QueryList<QuickViewComponent>;
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  onSelection(selected: QuickViewComponent) {
+    this.quickViews.forEach(item => {
+      if (item !== selected) {
+        item.closeDetail();
+      }
+    });
+  }
 }
--- front/src/app/components/quick-view/quick-view.component.ts
+++ front/src/app/components/quick-view/quick-view.component.ts
@@ -1,18 +1,29 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {AssetModel}               from '../../models/search/asset.model';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {AssetModel}                             from '../../models/search/asset.model';
 
-@Component({
-  selector: 'app-quick-view',
-  templateUrl: './quick-view.component.html',
-  styleUrls: ['./quick-view.component.css']
-})
+@Component(
+  {
+    selector:    'app-quick-view',
+    templateUrl: './quick-view.component.html',
+    styleUrls:   ['./quick-view.component.css']
+  }
+)
 export class QuickViewComponent {
 
   @Input() item: AssetModel;
+  @Output() opening = new EventEmitter<QuickViewComponent>();
 
-  public displayDetail = false;
+  public displayDetail: boolean = false;
 
   toggleDetail() {
     this.displayDetail = !this.displayDetail;
+
+    if (this.displayDetail){
+      this.opening.emit(this);
+    }
+  }
+
+  closeDetail() {
+    this.displayDetail = false;
   }
 }
