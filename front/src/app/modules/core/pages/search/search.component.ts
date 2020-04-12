e2f9e8ecde44a59b064182b68827f65f783b0911
John Daligault <johndaligault@gmail.com>
2020-04-12 19:17:32 +0200

    updating empty result page

--- front/src/app/modules/core/pages/search/search.component.ts
+++ front/src/app/modules/core/pages/search/search.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {ApiService} from '../../../../services/api.service';
 import {AssetModel} from '../../../../models/search/asset.model';
 import {QuickViewComponent} from '../../components/quick-view/quick-view.component';
@@ -6,173 +6,179 @@ import {MatDialog} from '@angular/material/dialog';
 import {HeaderComponent} from '../../components/header/header.component';
 
 @Component(
-    {
-        selector:    'app-assets-search',
-        templateUrl: './search.component.html',
-        styleUrls:   ['./search.component.scss']
-    }
+  {
+    selector:    'app-assets-search',
+    templateUrl: './search.component.html',
+    styleUrls:   ['./search.component.scss']
+  }
 )
 export class SearchComponent implements OnInit {
 
-    @ViewChild('headerComponent', {static: true}) private headerComponent: HeaderComponent;
+  @ViewChild('headerComponent', {static: true}) private headerComponent: HeaderComponent;
 
 
-    displayFilterMenu = false;
-    blurry            = false;
+  displayFilterMenu = false;
+  blurry            = false;
 
-    result: AssetModel[] = [];
+  result: AssetModel[] = [];
 
-    recent: AssetModel[] = [];
+  heading: AssetModel;
+  recent: AssetModel[] = [];
 
-    types: {
-        film: AssetModel[],
-        shortFilm: AssetModel[],
-        series: AssetModel[],
-        documentary: AssetModel[],
+  types: {
+    film: AssetModel[],
+    shortFilm: AssetModel[],
+    series: AssetModel[],
+    documentary: AssetModel[],
+  };
+
+  categories: {
+    humour: AssetModel[]
+    aventure: AssetModel[]
+    thriller: AssetModel[]
+    drames: AssetModel[]
+    scienceFiction: AssetModel[]
+    rommantique: AssetModel[]
+    action: AssetModel[]
+    horreur: AssetModel[]
+  };
+
+  filters: {} = {};
+
+  query = '';
+
+  activeFilters = {
+    locale:     null,
+    type:       null,
+    categories: null,
+  };
+
+  constructor(
+    private api: ApiService,
+    public dialog: MatDialog
+  ) {
+    this.types = {
+      film:        [],
+      shortFilm:   [],
+      series:      [],
+      documentary: [],
     };
 
-    categories: {
-        humour: AssetModel[]
-        aventure: AssetModel[]
-        thriller: AssetModel[]
-        drames: AssetModel[]
-        scienceFiction: AssetModel[]
-        rommantique: AssetModel[]
-        action: AssetModel[]
-        horreur: AssetModel[]
+    this.categories = {
+      humour:         [],
+      aventure:       [],
+      thriller:       [],
+      drames:         [],
+      scienceFiction: [],
+      rommantique:    [],
+      action:         [],
+      horreur:        [],
     };
+  }
 
-    filters: {} = {};
+  ngOnInit() {
+    this.makeSearch();
+  }
 
-    query = '';
+  toogleFilterMenu() {
+    this.displayFilterMenu = !this.displayFilterMenu;
+  }
 
-    activeFilters = {
-        locale:     null,
-        type:       null,
-        categories: null,
-    };
+  makeSearch(): void {
+    this.api.searchSeries(this.query, this.activeFilters).subscribe(data => {
+      this.result  = data.assets;
+      this.filters = data.filters;
 
-    constructor(
-        private api: ApiService,
-        public dialog: MatDialog
-    ) {
-        this.types = {
-            film:        [],
-            shortFilm:   [],
-            series:      [],
-            documentary: [],
-        };
+      this.recent = this.getRecent(this.result);
 
-        this.categories = {
-            humour:         [],
-            aventure:       [],
-            thriller:       [],
-            drames:         [],
-            scienceFiction: [],
-            rommantique:    [],
-            action:         [],
-            horreur:        [],
-        };
+      if (this.heading === undefined) {
+        this.heading = this.recent[3];
+      }
+
+      this.types.series      = this.getByType(this.result, 'Séries');
+      this.types.film        = this.getByType(this.result, 'Film');
+      this.types.shortFilm   = this.getByType(this.result, 'Court métrage');
+      this.types.documentary = this.getByType(this.result, 'Documentaire');
+
+      this.categories.humour         = this.getByCategories(this.result, 'Humour');
+      this.categories.aventure       = this.getByCategories(this.result, 'Aventure');
+      this.categories.thriller       = this.getByCategories(this.result, 'Thriller');
+      this.categories.drames         = this.getByCategories(this.result, 'Drames');
+      this.categories.scienceFiction = this.getByCategories(this.result, 'Science fiction');
+      this.categories.rommantique    = this.getByCategories(this.result, 'Rommantique');
+      this.categories.action         = this.getByCategories(this.result, 'Action');
+      this.categories.horreur        = this.getByCategories(this.result, 'Horreur');
+    });
+  }
+
+  onFiltersChange(): void {
+    this.makeSearch();
+  }
+
+  clearActiveFilters(): void {
+    this.activeFilters.locale     = null;
+    this.activeFilters.type       = null;
+    this.activeFilters.categories = null;
+    this.query                    = '';
+    this.makeSearch();
+  }
+
+  hasActiveFilters(): boolean {
+    let activeFilter = false;
+
+    for (const [, value] of Object.entries(this.activeFilters)) {
+      if (value) {
+        activeFilter = true;
+      }
     }
 
-    ngOnInit() {
-        this.makeSearch();
-    }
+    return activeFilter;
+  }
 
-    toogleFilterMenu() {
-        this.displayFilterMenu = !this.displayFilterMenu;
-    }
+  updateQuery(query: string) {
+    this.query = query;
+    this.makeSearch();
+  }
 
-    makeSearch(): void {
-        this.api.searchSeries(this.query, this.activeFilters).subscribe(data => {
-            this.result  = data.assets;
-            this.filters = data.filters;
+  getRecent(series: AssetModel[]): AssetModel[] {
+    return series
+      .sort((a, b) => b.import_date - a.import_date);
+  }
 
-            this.recent = this.getRecent(this.result);
+  getByType(series: AssetModel[], type: string) {
+    return series
+      .filter((a) => a.type === type)
+      .sort((a, b) => {
+        return 0.5 - Math.random();
+      });
+  }
 
-            this.types.series      = this.getByType(this.result, 'Séries');
-            this.types.film        = this.getByType(this.result, 'Film');
-            this.types.shortFilm   = this.getByType(this.result, 'Court métrage');
-            this.types.documentary = this.getByType(this.result, 'Documentaire');
+  getByCategories(series: AssetModel[], category: string) {
+    return series
+      .filter((a) => a.categories.includes(category))
+      .sort((a, b) => {
+        return 0.5 - Math.random();
+      });
+  }
 
-            this.categories.humour         = this.getByCategories(this.result, 'Humour');
-            this.categories.aventure       = this.getByCategories(this.result, 'Aventure');
-            this.categories.thriller       = this.getByCategories(this.result, 'Thriller');
-            this.categories.drames         = this.getByCategories(this.result, 'Drames');
-            this.categories.scienceFiction = this.getByCategories(this.result, 'Science fiction');
-            this.categories.rommantique    = this.getByCategories(this.result, 'Rommantique');
-            this.categories.action         = this.getByCategories(this.result, 'Action');
-            this.categories.horreur        = this.getByCategories(this.result, 'Horreur');
-        });
-    }
+  onSelection(series: AssetModel) {
+    this.blurry = true;
+    this.headerComponent.pause();
 
-    onFiltersChange(): void {
-        this.makeSearch();
-    }
-
-    clearActiveFilters(): void {
-        this.activeFilters.locale     = null;
-        this.activeFilters.type       = null;
-        this.activeFilters.categories = null;
-        this.makeSearch();
-    }
-
-    hasActiveFilters(): boolean {
-        let activeFilter = false;
-
-        for (const [, value] of Object.entries(this.activeFilters)) {
-            if (value) {
-                activeFilter = true;
-            }
+    this
+      .dialog
+      .open(
+        QuickViewComponent,
+        {
+          maxWidth: '800px',
+          minWidth: '300px',
+          data:     series
         }
-
-        return activeFilter;
-    }
-
-    updateQuery(query: string) {
-        this.query = query;
-        this.makeSearch();
-    }
-
-    getRecent(series: AssetModel[]): AssetModel[] {
-        return series
-            .sort((a, b) => b.import_date - a.import_date);
-    }
-
-    getByType(series: AssetModel[], type: string) {
-        return series
-            .filter((a) => a.type === type)
-            .sort((a, b) => {
-                return 0.5 - Math.random();
-            });
-    }
-
-    getByCategories(series: AssetModel[], category: string) {
-        return series
-            .filter((a) => a.categories.includes(category))
-            .sort((a, b) => {
-                return 0.5 - Math.random();
-            });
-    }
-
-    onSelection(series: AssetModel) {
-        this.blurry = true;
-        this.headerComponent.pause();
-
-        this
-            .dialog
-            .open(
-                QuickViewComponent,
-                {
-                    maxWidth: '800px',
-                    minWidth: '300px',
-                    data:     series
-                }
-            )
-            .afterClosed()
-            .subscribe(() => {
-                this.blurry = false;
-                this.headerComponent.play();
-            });
-    }
+      )
+      .afterClosed()
+      .subscribe(() => {
+        this.blurry = false;
+        this.headerComponent.play();
+      });
+  }
 }
