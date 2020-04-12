f4e8baa066c2671874add369923aa1aca38c412d
John Daligault <johndaligault@gmail.com>
2020-04-12 20:43:51 +0200

    fixing phpmd

--- back/src/Enum/Enum.php
+++ back/src/Enum/Enum.php
@@ -7,14 +7,13 @@ namespace App\Enum;
 use ReflectionClass;
 use RuntimeException;
 use ReflectionException;
-use Doctrine\DBAL\Types\Type;
 use InvalidArgumentException;
 use Doctrine\DBAL\Platforms\SqlitePlatform;
 use Doctrine\DBAL\Platforms\AbstractPlatform;
 use Doctrine\DBAL\Platforms\SQLServerPlatform;
 use Doctrine\DBAL\Platforms\PostgreSqlPlatform;
 
-abstract class Enum extends Type
+abstract class Enum
 {
     /**
      * @var array
@@ -69,83 +68,5 @@ abstract class Enum extends Type
     {
         $values = array_values(self::getAll());
         return in_array($value, $values, true);
-    }
-
-    /**
-     * @param AbstractPlatform $platform
-     * @param array            $fieldDeclaration
-     *
-     * @return string
-     */
-    public function getSqlDeclaration(array $fieldDeclaration, AbstractPlatform $platform)
-    {
-        // wrapping values
-        $values = array_map(function ($val) {
-            return "'" . $val . "'";
-        }, self::getAll());
-
-        switch (true) {
-            case $platform instanceof SqlitePlatform:
-                $sqlDeclaration = sprintf(
-                    'TEXT CHECK(%s IN (%s))',
-                    $fieldDeclaration['name'],
-                    implode(', ', $values)
-                );
-                break;
-            case $platform instanceof PostgreSqlPlatform:
-            case $platform instanceof SQLServerPlatform:
-                $sqlDeclaration = sprintf(
-                    'VARCHAR(255) CHECK(%s IN (%s))',
-                    $fieldDeclaration['name'],
-                    implode(', ', $values)
-                );
-
-                break;
-            default:
-                $sqlDeclaration = sprintf('ENUM(%s)', implode(", ", $values));
-        }
-
-        return $sqlDeclaration;
-    }
-
-    /**
-     * @param mixed            $value
-     * @param AbstractPlatform $platform
-     *
-     * @return mixed
-     */
-    public function convertToPHPValue($value, AbstractPlatform $platform)
-    {
-        return $value;
-    }
-
-    /**
-     * @param AbstractPlatform $platform
-     * @param mixed            $value
-     *
-     * @return mixed
-     */
-    public function convertToDatabaseValue($value, AbstractPlatform $platform)
-    {
-        if (!in_array($value, self::getAll(), true)) {
-            throw new InvalidArgumentException(sprintf("Invalid '%s' value.", $this->getName()));
-        }
-        return $value;
-    }
-
-    /**
-     * Return the ClassName without namespace prefix
-     *
-     * @return string
-     */
-    public function getName()
-    {
-        try {
-            $refection = new ReflectionClass($this);
-            return $refection->getShortName();
-        } catch (ReflectionException $e) {
-            // avoid throwing reflection exception
-            throw new RuntimeException($e->getMessage(), $e->getCode(), $e);
-        }
     }
 }
